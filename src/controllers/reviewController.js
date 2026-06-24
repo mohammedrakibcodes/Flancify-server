ea1afe6a30eb145e453a86a753a7463a2825fc55
Mohammed Rakib <mohammedrakibhossain02@gmail.com>
2026-06-24 18:34:12 +0600

    Add review validation and duplicate review protection

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,8 +1,55 @@
-const { reviewsCollection } = require("../db/collections");
+const { ObjectId } = require("mongodb");
+
+const {
+  reviewsCollection,
+  tasksCollection,
+  paymentsCollection,
+} = require("../db/collections");
 
 const createReview = async (req, res) => {
   try {
     const reviewData = req.body;
+
+    const task = await tasksCollection.findOne({
+      _id: new ObjectId(reviewData.task_id),
+    });
+
+    if (!task) {
+      return res.status(404).send({
+        success: false,
+        message: "Task not found",
+      });
+    }
+
+    if (task.status !== "completed") {
+      return res.status(400).send({
+        success: false,
+        message: "Task not completed yet",
+      });
+    }
+
+    const payment = await paymentsCollection.findOne({
+      taskId: reviewData.task_id,
+    });
+
+    if (!payment) {
+      return res.status(400).send({
+        success: false,
+        message: "Payment not found",
+      });
+    }
+
+    const existingReview = await reviewsCollection.findOne({
+      task_id: reviewData.task_id,
+      reviewer_email: reviewData.reviewer_email,
+    });
+
+    if (existingReview) {
+      return res.status(409).send({
+        success: false,
+        message: "You already reviewed this task",
+      });
+    }
 
     reviewData.created_at = new Date();
 
